8167285d5a9c8ab860ea09ae20ce5a6d4a84fe30
jmarcosra <>
2023-07-08 03:30:55 +0200

    Primer commit de HolaMundoGit

---
+++ src/holamundogit/HolaMundoGit.java
@@ -0,0 +1,19 @@
+package holamundogit;
+
+/**
+ * Clase que implementa el típico "Hola mundo" para usar con Git
+ */
+public class HolaMundoGit
+{
+
+	/**
+	 * Función principal del programa
+	 * 
+	 * @param	args	argumentos pasados desde la línea de comandos
+	 */
+	public static void main (String[] args)
+	{
+		System.out.println("Hola mundo Git");
+	}
+	
+}
